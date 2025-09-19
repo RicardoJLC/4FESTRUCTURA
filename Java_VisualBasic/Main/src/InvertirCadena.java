1d4921bbb5b5436e0ca0156a81d920eb7779d2d3
ricardojaciellozachavez <20243ds179@utez.edu.mx>
2025-09-19 07:51:09 -0600

    Realize una impresion por consola de una cadena de caracteres invertida

---
+++ Java_VisualBasic/Main/src/InvertirCadena.java
@@ -0,0 +1,16 @@
+public class InvertirCadena {
+    // Con esto se imprimre la cadena al revés usando substring
+    // el metodo subtraing es una operacion del String que no es un tipo de dato
+    // primitivo sino una clase o objeto
+    public static void invertir(String s) {
+        if (s == null || s.length() == 0) return;   // <-- caso base
+        System.out.print(s.charAt(s.length() - 1));  // <-- imprime último carácter
+        invertir(s.substring(0, s.length() - 1));   // <-- recursión con resto de la cadena
+    }
+
+    public static void main(String[] args) {
+        String texto = "hola";
+        invertir(texto);         // salida: aloh
+        System.out.println();    // salto de línea al final
+    }
+}
