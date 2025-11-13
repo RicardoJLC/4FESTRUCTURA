45470070cf7f1aad69e1f8ca853fce4eb2ddd384
Ricardo Jaciel Loza Chávez <20243ds179@utez.edu.mx>
2025-11-13 08:21:32 -0600

    Add files via upload

---
+++ Insertionsort/INSERTIONSORT/src/App.java
@@ -0,0 +1,7 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        int[] arr = {10,3,5,1,7};
+        InsertionSort sorter = new InsertionSort();
+        sorter.sort(arr);
+    }
+}
